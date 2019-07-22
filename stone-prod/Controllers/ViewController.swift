4749dd59c735dbc77aef92b4f0b59f08c59097e7
Sunjeev Guru <sunjeevgururangan@gmail.com>
2019-07-21 22:53:49 -0400

    added launch screen

---
+++ stone-prod/Controllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  stone-prod
+//
+//  Created by Abu Swaminathan on 7/21/19.
+//  Copyright © 2019 abuswaminathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var goBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        goBtn.layer.cornerRadius = 8
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
